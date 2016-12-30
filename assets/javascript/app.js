30bd4a7c078c33287b46dd95cc89744ac2ffb8a4
Justin Carlson <justin.ryan.carlson@gmail.com>
2016-12-30 16:13:17 -0600

    adds method in ClozeFlashCard contructor to return only the cloze card's text

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,8 +11,11 @@ function ClozeFlashCard(text, cloze, type) {
     this.clozeCardText = text;
     this.clozeCardCloze = cloze;
     this.clozeCardType = type;
-    this.clozeReturn = function() {
+    this.clozeReturnCloze = function() {
         return this.clozeCardCloze;
+    };
+    this.clozeReturnText = function() {
+        return this.clozeCardText;
     };
 }
 
