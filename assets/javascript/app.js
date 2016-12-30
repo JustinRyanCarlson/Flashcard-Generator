abcfafeaaba7b5b34d268e34c78b9047906ef2cb
Justin Carlson <justin.ryan.carlson@gmail.com>
2016-12-30 17:30:47 -0600

    removes the instructions from app.js and moves them to the README

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,28 +15,6 @@ var cardClozeText;
 
 
 
-// 5: Constructor and prototype used to hold methods for basic flash cards
-// 5: Constructor and prototype used to hold basic methods for cloze-deleted flash cards
-// 5: Properly fill out readme with examples of each constructor and the methods (use request's npm page as an example of the depth we're looking for)
-// 5: 2 modules used to hold each constructor, and a 3rd file used as a single entry point which exports both.
-// 5: ClozeCard has property or method that returns only the cloze-deleted portion of the text
-// 5: ClozeCard has property or method that returns only the partial text
-// 5: ClozeCard throws error if it is not able to figure out where the cloze-deletion should go.
-
-console.log('');
-console.log('Hello! Welcome to my flashcard node.js app');
-console.log('');
-console.log('To use this application use the arguments:');
-console.log('1. node');
-console.log('2. Path to app.js file');
-console.log('3. Which type of flashcard you would like to create (basic or cloze)');
-console.log('4. The front then back of the basic flashcard with a comma seperating them OR the cloze text with the statement you want to cloze delete inside curly braces ({})');
-console.log('');
-console.log('Example: node app.js basic Who was the first president of the United States, George Washington');
-console.log('');
-
-
-
 //
 // BASE CODE
 //
